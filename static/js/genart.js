d195a795e135da7cc98f60b5520d1c07633d7da4
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2023-08-18 21:27:28 +0200

    fix id name

--- static/js/genart.js
+++ static/js/genart.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("landing-background");
+const canvas = document.getElementById("landing-canva");
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "#EEEEEE";
