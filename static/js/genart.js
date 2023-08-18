38c3e6fc6165a9230bd78e606a4e9e2d94d8f782
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2023-08-18 21:26:45 +0200

    split landing structure

--- static/js/genart.js
+++ static/js/genart.js
@@ -2,4 +2,5 @@ const canvas = document.getElementById("landing-background");
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "#EEEEEE";
-ctx.fillRect(10, 30, 150, 75);
+ctx.fillRect(20, 30, 150, 75);
+ctx.fillRect(5, 0, 150, 75);
